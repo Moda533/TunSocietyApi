ba9d477b3ed645b092c20c1145653aa0858a518f
unicontact <omriahmed019@gmail.com>
2026-05-21 14:06:59 +0100

    disable migration startup crash

--- Program.cs
+++ Program.cs
@@ -144,13 +144,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext =
-        scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    dbContext.Database.Migrate();
-}
+// Database migration disabled on Render startup
 
 app.UseCors("Frontend");
 
